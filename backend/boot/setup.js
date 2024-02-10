006f40199a1327fe4e9747f22673d8c04db848b3
dnlmor <morenosiahaan07@gmail.com>
2024-02-10 20:21:30 +0100

    changes

--- backend/boot/setup.js
+++ backend/boot/setup.js
@@ -16,7 +16,7 @@ const PORT = 8080;
 // MongoDB connection
 async function connectToMongoDB() {
   try {
-    const uri = 'mongodb+srv://account:<password>@cluster0.sfbjbpt.mongodb.net/?retryWrites=true&w=majority';
+    const uri = 'mongodb+srv://dnlmor:ahgase@cluster0.sfbjbpt.mongodb.net/?retryWrites=true&w=majority';
 
     await mongoose.connect(uri);
 
