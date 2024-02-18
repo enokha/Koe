a8a1f369fa8d2225f274b270a2cc32b746945d14
eliasoooo <elias-ghanem@outlook.com>
2024-02-18 23:57:06 +0100

    final update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import PrivateRoute from './routes/PrivateRoute';
-import PublicRoute from './routes/PublicRoute';
 import ExplorePage from './pages/ExplorePage';
 import NotificationPage from './pages/NotificationPage';
 import MessagesPage from './pages/MessagesPage';
@@ -12,19 +10,17 @@ import UserProfile from './pages/UserProfile';
 function App() {
   return (
     <Routes>
-      <Route path="/login/*" element={<PublicRoute />} />
-      {/* PrivateRoute for home page (requires authentication) */}
-      <Route path="/home/*" element={<PrivateRoute element={<HomePage />} />}>
-        {/* Additional PrivateRoutes for other pages */}
-        <Route path="explore/*" element={<ExplorePage />} />
-        <Route path="notifications/*" element={<NotificationPage />} />
-        <Route path="messages/*" element={<MessagesPage />} />
-        <Route path="profile/*" element={<UserProfile />} />
-      </Route>
+      {/* Public route for login */}
+      <Route path="/login/*" element={<LoginPage />} />
+      {/* Public route for home */}
+      <Route path="/home/*" element={<HomePage />} />
+      {/* Private routes for other pages */}
+      <Route path="/explore/*" element={<ExplorePage />} />
+      <Route path="/notifications/*" element={<NotificationPage />} />
+      <Route path="/messages/*" element={<MessagesPage />} />
+      <Route path="/profile/*" element={<UserProfile />} />
       {/* Default route to redirect to login */}
       <Route path="*" element={<Navigate to="/login" />} />
-      {/* Additional route for LoginPage */}
-      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
